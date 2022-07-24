9e6b564ab5ff6149024621b69821a16d7b0bf548
Jaruwan Saeaei <84604436+minimintorz@users.noreply.github.com>
2022-07-24 21:27:24 +0700

    add_contact

--- lib/profile.dart
+++ lib/profile.dart
@@ -6,30 +6,92 @@ class profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(5.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(
-              height: 100,
+            const SizedBox(
+              height: 150,
             ),
             Container(
-              width: 200,
-              height: 200,
-              padding: EdgeInsets.all(5),
-              decoration: BoxDecoration(),
+              width: 225,
+              height: 225,
+              padding: const EdgeInsets.fromLTRB(20, 0, 0, 20),
+              decoration: const BoxDecoration(
+                shape: BoxShape.rectangle,
+              ),
               child: Image.asset(
                 'images/ProfilePic.jpg',
                 fit: BoxFit.cover,
                 // width: 200,
-                height: 300,
+                //height: 500,
               ),
             ),
-            Text('Mintira Sae-eia',style: TextStyle(
-                fontFamily: 'Oswald',
-                fontSize: 50,
-            ),),
+            Container(
+              padding: const EdgeInsets.fromLTRB(20, 0, 0, 20),
+              child: const Text(
+                'MS.Mintira Sae-Eia (Mint)',
+                style: TextStyle(
+                  fontFamily: 'Oswald',
+                  fontSize: 30,
+                  letterSpacing: 2,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 18, 0, 107),
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.fromLTRB(20, 0, 0, 10),
+              child: Row(
+                children: const [
+                  Icon(
+                    Icons.email_outlined,
+                    size: 40,
+                    color: Colors.blueGrey,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      '6240011003@psu.ac.th',
+                      style: TextStyle(
+                        fontFamily: 'Oswald',
+                        fontSize: 18,
+                        letterSpacing: 2,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 0, 36, 65),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.fromLTRB(20, 0, 0, 10),
+              child: Row(
+                children: const [
+                  Icon(
+                    Icons.call_outlined,
+                    size: 40,
+                    color: Colors.blueGrey,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      '+66 93-641-7136',
+                      style: TextStyle(
+                        fontFamily: 'Oswald',
+                        fontSize: 18,
+                        letterSpacing: 2,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 0, 36, 65),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
